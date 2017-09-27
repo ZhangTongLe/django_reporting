b4767cc9e2e82b5af477a35d839359fed5314767
Clark Xu <xu_chl@163.com>
2017-09-27 09:30:07 +0800

    模板

---
+++ django-docs-1.10-en/Item-Topic guides/7.templates.py
@@ -0,0 +1,30 @@
+ A template contains the static parts of the desired HTML output as well as some special syntax describing how dynamic content will be inserted
+
+ 一.例子
+ polls/templates/polls/index.html
+{% if latest_question_list %}
+    <ul>
+    {% for question in latest_question_list %}
+        <li><a href="/polls/{{ question.id }}/">{{ question.question_text }}</a></li>
+    {% endfor %}
+    </ul>
+{% else %}
+    <p>No polls are available.</p>
+{% endif %}
+
+
+polls/views.py
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
